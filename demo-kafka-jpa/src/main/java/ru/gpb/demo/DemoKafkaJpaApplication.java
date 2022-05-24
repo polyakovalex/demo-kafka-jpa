28360cfa22828354e1a95e0d3ecd39d128de8881
Поляков Алексей <poljakov347@gmail.com>
2022-05-24 18:03:18 +0300

    прототип kafka

---
+++ demo-kafka-jpa/src/main/java/ru/gpb/demo/DemoKafkaJpaApplication.java
@@ -0,0 +1,13 @@
+package ru.gpb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoKafkaJpaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(DemoKafkaJpaApplication.class, args);
+  }
+
+}
