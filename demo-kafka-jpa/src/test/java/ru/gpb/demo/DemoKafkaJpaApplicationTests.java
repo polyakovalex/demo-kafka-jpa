4d369c428517fe20073c843669437ff82d1ab3d9
Поляков Алексей <poljakov347@gmail.com>
2022-05-24 18:04:19 +0300

    прототип

--- demo-kafka-jpa/src/test/java/ru/gpb/demo/DemoKafkaJpaApplicationTests.java
+++ demo-kafka-jpa/src/test/java/ru/gpb/demo/DemoKafkaJpaApplicationTests.java
@@ -1,13 +1,48 @@
 package ru.gpb.demo;
 
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.gpb.demo.entity.EmailEntity;
+import ru.gpb.demo.kafka.KafkaSenderSync;
+import ru.gpb.demo.model.Client;
+import ru.gpb.demo.repository.EmailRepository;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class DemoKafkaJpaApplicationTests {
 
+  @Autowired
+  EmailRepository emailRepository;
+
+  @Autowired
+  KafkaSenderSync kafkaSenderSync;
+
+/*  @Autowired
+  KafkaListenersExample kafkaListenersExample;*/
+
   @Test
-  void contextLoads() {
+  void getTableTest() {
+    List<EmailEntity> emailEntities = emailRepository.findAll();
+    System.out.println("TEST");
   }
+
+  @Test
+  void sendKafkaSync(){
+    Client client = new Client(0.0, "Vasya", "Pupkin");
+    kafkaSenderSync.sendMessage("werfwef", "IN");
+    System.out.println("Send");
+  }
+
+/*
+  @Test
+  void listenerKafkaS(){
+    kafkaListenersExample.commonListenerForInTopics("Test");
+    System.out.println("Send");
+  }
+*/
 
 }
